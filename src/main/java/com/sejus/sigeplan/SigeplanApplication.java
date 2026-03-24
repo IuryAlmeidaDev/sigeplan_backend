089b75eb06495cc0ac25218ea3645b426bd2fab1
IuryAlmeidaDev <iury.colombo@gmail.com>
2026-03-23 21:12:54 -0400

    first commit

---
+++ src/main/java/com/sejus/sigeplan/SigeplanApplication.java
@@ -0,0 +1,13 @@
+package com.sejus.sigeplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigeplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigeplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sejus/sigeplan/SigeplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejus.sigeplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigeplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
